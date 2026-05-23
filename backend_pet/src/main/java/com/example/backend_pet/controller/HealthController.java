1b02e0d4adf9ca911d08f0cc26f0e9d9b949675b
unknown <khanhduong290409@gmail.com>
2026-05-23 12:15:05 +0700

    fix lai api heath

--- backend_pet/src/main/java/com/example/backend_pet/controller/HealthController.java
+++ backend_pet/src/main/java/com/example/backend_pet/controller/HealthController.java
@@ -17,7 +17,7 @@ public class HealthController {
     // GET /api/health - ping giữ Render và Neon DB cùng sống
     @GetMapping("/api/health")
     public ResponseEntity<Map<String, String>> health() {
-        userRepository.count();
+        // userRepository.count();// viết 1 lệnh trích xuất vào db để check xem có giải quyết được vấn đề backend xử lý chậm sau khi deploy không
         return ResponseEntity.ok(Map.of("status", "ok"));
     }
 }
